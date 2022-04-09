f81d437216f39a630cd4518c52930fd2149d3bd8
原遼太朗 <ryotarohara@hararyoutarounoMacBook-Pro.local>
2022-04-09 09:19:09 +0900

    Rspecテスト導入

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,11 @@ module TaskApp
     # config.eager_load_paths << Rails.root.join("extras")
     config.action_controller.permit_all_parameters = true
     config.time_zone = 'Tokyo'
+    config.generators do |g|
+      g.test_framework :rspec,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false
+    end
   end
 end
